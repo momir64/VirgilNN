6e48649f99b0cfe66f19b7e7f0bd5890e193b4f2
Momir Stanišić <momir.stanisic.ns@gmail.com>
2025-08-31 02:43:01 +0200

    fix: added load points switch and fixed projection switch

--- src/visualization/map_plotter.py
+++ src/visualization/map_plotter.py
@@ -5,22 +5,24 @@ import geopandas as gpd
 import json
 
 PROJECTION_TYPE = "EPSG:4326" if False else "EPSG:3035"
-ENUMERATE_CELLS = False
+ENUMERATE_CELLS = True
+LOAD_POINTS = False
 
-with open(ALL_LOCATIONS_PATH) as file:
-    grouped_locations = json.load(file)
-    points = []
-    for locations in grouped_locations.values():
-        for location in locations:
-            points.append(Point(location["lng"], location["lat"]))
-
-points_gdf = gpd.GeoDataFrame(geometry=points, crs="EPSG:4326")
 europe = gpd.read_file(EUROPE_PATH).to_crs(PROJECTION_TYPE)
 grid = gpd.read_file(GRID_PATH).to_crs(PROJECTION_TYPE)
 
 ax = europe.plot(figsize=(12, 10), color="lightgreen", edgecolor="black")
-points_gdf.plot(ax=ax, color="darkgreen", markersize=0.1)
 grid.boundary.plot(ax=ax, color="black", linewidth=0.5)
+
+if LOAD_POINTS:
+    with open(ALL_LOCATIONS_PATH) as file:
+        grouped_locations = json.load(file)
+        points = []
+        for locations in grouped_locations.values():
+            for location in locations:
+                points.append(Point(location["lng"], location["lat"]))
+    points_gdf = gpd.GeoDataFrame(geometry=points, crs="EPSG:4326").to_crs(PROJECTION_TYPE)
+    points_gdf.plot(ax=ax, color="darkgreen", markersize=0.1)
 
 if ENUMERATE_CELLS:
     for idx, row in grid.iterrows():
